a3f1b0a87141c64ab0d680dd5b44590b8912726b
AbdullahAlsharif <39487439+AbdullahAlsharif@users.noreply.github.com>
2018-05-23 15:05:47 +0300

    First C# Program
    This is my frist C# Program

---
+++ CSharpCourseCodes/FirstVariabl/FirstVariabl/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstVariabl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 25;
+            Console.WriteLine("My Variable = {0}", x);
+            Console.ReadKey();
+        }
+    }
+}
