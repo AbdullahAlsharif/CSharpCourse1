1ac4abe1e5a5fc184136a4b46a0b3718d794495d
AbdullahAlsharif <39487439+AbdullahAlsharif@users.noreply.github.com>
2018-05-30 14:56:02 +0300

    Cahnge Location

---
+++ Expressions/Expressions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float myVar = 10.5f;
+            float myVar2 = 10.7f;
+
+            float temp = myVar + myVar2;
+
+            /* 1- myVar = 10.5f & myVar2 = 10.7f تسمى أوبرنت ; 
+               2- (+) (علامة الجمع والطرح والقسمة والضرب تسمى أوبيريتر); 
+               3- (float temp = myVar + myVar2;) تسمى اكسبريشن
+             */
+             // Basic operators (+ - * /)
+             // (العمليات الصعبة) bitwise operations (التحويل الثنائي)
+             // Logical operators Boolean (true, false)
+
+            /* المعاملات تنفسم إلى ثلاثة أنواع
+             1_ Unary operand المعاملات الأحادية
+             2_ binary operands المعاملات الثنائية
+             3_ 3 operands المعاملات الثلاثية (لدينا حالة واحدة فقط) الشرطية وسيتم توضيحها لاحقا
+             */
+
+
+        }
+    }
+}
