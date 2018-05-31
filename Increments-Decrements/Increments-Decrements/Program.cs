337bc9d70d46e864cc1b3cc574e13f8db42a7778
AbdullahAlsharif <39487439+AbdullahAlsharif@users.noreply.github.com>
2018-05-31 15:05:41 +0300

    Increments - Decrements

---
+++ Increments-Decrements/Increments-Decrements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Increments_Decrements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar1 = 2;
+            int Temp1 = ++myVar1;
+            // وضع علامة الجمع قبل المتغير تعني زيادة للقيمة بمقدار (1) فقط ++ أما وضعها بعد المتغير فلا يغير النتيجة
+            Console.WriteLine("Temp1 = {0} ", Temp1);
+
+            int myVar2 = 2;
+            int Temp2 = --myVar2;
+            // وكذلك وضع علامة الناقص قبل المتغير تنقص واحد من العدد
+            Console.WriteLine("Temp2 = {0} ", Temp2);
+
+            int myVar3 = 7;
+            int myVar4 = 2;
+            int Temp3 = myVar3-- / ++myVar4;
+            Console.WriteLine("Temp3 = {0}", Temp3);
+            Console.ReadKey();
+        }
+    }
+}
--- Increments-Decrements/Increments-Decrements/Properties/AssemblyInfo.cs
+++ Increments-Decrements/Increments-Decrements/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Expressions")]
+[assembly: AssemblyTitle("Increments-Decrements")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Expressions")]
+[assembly: AssemblyProduct("Increments-Decrements")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("17ca7581-fb0a-4259-ac5f-f593af1f6798")]
+[assembly: Guid("c735cd34-a65d-4fbe-a8ad-478a1fdd23cf")]
 
 // Version information for an assembly consists of the following four values:
 //
