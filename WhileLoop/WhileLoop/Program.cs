54de127a0065f639ab0af7feeb07ab2c01927867
AbdullahAlsharif <39487439+AbdullahAlsharif@users.noreply.github.com>
2018-07-18 09:56:07 +0300

    While Loop

---
+++ WhileLoop/WhileLoop/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int i = 0;
+             double mySalary = 1020.30, total = 0.0;
+             while (i < 15)
+             {
+                 total += mySalary;
+                 i++;
+             }
+             Console.WriteLine("My Salary is {0}", total);
+
+             Console.ReadKey();
+             */
+            int i = 0;
+            int myWeight = 120;
+            while (myWeight > 80)
+            {
+                myWeight--;
+                i++;
+            }
+            Console.WriteLine("Number of Exercises is {0}", i);
+            Console.ReadKey();
+        }
+    }
+}
