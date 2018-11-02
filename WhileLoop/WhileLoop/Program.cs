97023de3da79dc1f409435ef6d6ee2861edc701b
AbdullahAlsharif <39487439+AbdullahAlsharif@users.noreply.github.com>
2018-11-02 17:45:11 +0300

    Update WhileLoop/WhileLoop/Program.cs
    Updated

--- WhileLoop/WhileLoop/Program.cs
+++ WhileLoop/WhileLoop/Program.cs
@@ -10,26 +10,38 @@ namespace WhileLoop
     {
         static void Main(string[] args)
         {
-            /* int i = 0;
-             double mySalary = 1020.30, total = 0.0;
-             while (i < 15)
-             {
+            /*int i = 0;
+            double mySalary = 1020.30, total = 0.0;
+            while(i<15)
+                {
                  total += mySalary;
-                 i++;
-             }
-             Console.WriteLine("My Salary is {0}", total);
-
-             Console.ReadKey();
-             */
-            int i = 0;
-            int myWeight = 120;
-            while (myWeight > 80)
-            {
-                myWeight--;
                 i++;
+                
             }
-            Console.WriteLine("Number of Exercises is {0}", i);
+            Console.WriteLine("My Salary is {0}", total);
+            Console.ReadKey();*/
+            /*
+           int i = 0;
+           int myWeight = 120;
+           while (myWeight > 80)
+           {
+               myWeight--;
+               i++;
+           }
+           Console.WriteLine("Number of Exercises is {0}", i);
+           Console.ReadKey();*/
+
+            int Exersices = 0;
+            double waigth = 120.0, total = 0.0;
+            while (waigth > 83)
+            {
+                waigth--;
+                Exersices++;
+
+            }
+            Console.WriteLine("Number of Exercises is {0}", Exersices);
             Console.ReadKey();
         }
+
     }
 }
